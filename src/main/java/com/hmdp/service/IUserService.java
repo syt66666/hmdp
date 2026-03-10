a84c1f9cc3abe4027b5ded884a71de9d9a53d50b
syt66666 <112790217+syt66666@users.noreply.github.com>
2026-03-10 21:21:07 +0800

    新增功能短信登录

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 
@@ -22,4 +23,10 @@ public interface IUserService extends IService<User> {
      * @return 验证码发送结果
      */
     Result sendCode(String phone, HttpSession session);
+
+    /**
+     * 登录功能
+     * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
+     */
+    Result login(LoginFormDTO loginForm, HttpSession session);
 }
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,28 @@
+package com.hmdp.utils;
+
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    /**
+     * 在请求处理之前进行调用（Controller方法调用之前）
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(UserHolder.getUser() == null){
+            //没有 需要拦截
+            response.setStatus(401);
+            return false;
+        }
+        //有用户，放行
+        return true;
+    }
+
+}
