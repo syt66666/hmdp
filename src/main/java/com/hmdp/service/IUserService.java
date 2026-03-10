03b37875a4052ded7e7ab9ae2f5500f4ca052f43
syt66666 <112790217+syt66666@users.noreply.github.com>
2026-03-10 17:02:00 +0800

    新建黑马点评项目

---
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -0,0 +1,25 @@
+package com.hmdp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+public interface IUserService extends IService<User> {
+
+    /**
+     * 发送验证码
+     * @param phone 手机号
+     * @return 验证码发送结果
+     */
+    Result sendCode(String phone, HttpSession session);
+}
